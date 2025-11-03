b5da9dc6cdefa368f2b80abe6438278648960df1
pryharin_bohdan <pryharin.bohdan@gmail.com>
2025-11-03 21:03:51 +0200

    first workable version

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
-const buttons = document.querySelectorAll('#top-area div');
+const username = document.querySelector("#username");
+const repo = document.querySelector("#repo");
 
-const alert = document.querySelector(".aside-settings");
+const urlLink = document.querySelector("#url-link");
 
-const closeBtn = document.querySelector(".aside-settings svg");
+if (localStorage.getItem("githubURL") !== null) {
+  urlLink.value = localStorage.getItem("githubURL");
+}
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    button.classList.toggle('animate');
-    alert.classList.toggle('show');
-    setTimeout( () => {
-        button.classList.toggle('animate');
-    }, 300)
-  });
-});
+function send_data() {
+  let isCorrect = false;
+  if (username.value === "") {
+      if (urlLink.value !== "") {
+        localStorage.setItem("githubURL", urlLink.value);
+        isCorrect = true;
+      } else if (repo.value === "") {
+        alert("Введите username и имя репозитория (или URL)!");
+      } else {
+        alert("Введите username (или URL)!");
+      }
+  } else if (repo.value === "") {
+        alert("Введите имя репозитория (или URL)!");
+  } else {
+      localStorage.setItem("githubURL", `https://github.com/${username.value}/${repo.value}`);
+      isCorrect = true;
+  }
 
-closeBtn.addEventListener('click', () => {
-  alert.classList.toggle('show');
-});
+  if (isCorrect) {
+    location.replace("./file-name.html")
+  }
+}
 
-/*
-const style = document.createElement('style');
-style.textContent = '* { backdrop-filter: none !important; }';
-document.head.appendChild(style);*/
+function open_github_commits() {
+    window.open(`${localStorage.getItem('githubURL')}/commits/main`);
+    location.replace('./sha.html');
+}
+
+function download_sha() {
+  const sha = document.querySelector('#sha');
+  const fileName = document.querySelector('#file-name');
+  if (!(localStorage.getItem("SHA") === null || localStorage.getItem("fileName") === null)) {
+    sha.value = localStorage.getItem("SHA");
+    fileName.value = localStorage.getItem("fileName");
+  }
+  if (sha.value === "" || fileName.value === "") {
+    alert("Поля не могут быть пустыми!");
+  } else {
+    localStorage.setItem("SHA", sha.value);
+    localStorage.setItem("fileName", fileName.value);
+  }
+  window.open(`${localStorage.getItem("githubURL")}/raw/${localStorage.getItem("SHA")}/${localStorage.getItem("fileName")}`)
+}
